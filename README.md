39bdc811b6238dfb83ca7c782dae95a784045714
sformichella <sformichella@gmail.com>
2020-10-01 14:37:55 -0700

    add js functions

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # lab-03-rock-paper-scissors
 
 HTML 
+
 1) Title of game in a header
 2) Explanation of rules in using numbered list
    1) User begins by choosing which option, rock, paper, or scissors they want to play
+      * Need to be mutually exclusive
    2) User will then click the submit button to "throw" their choice
+      * On click, what happens?
+         - Throw the users choice
+            - Get the :checked input from the DOM 
+   3) Get computer's throw
+      * Get a random number from 0, 1, or 2
+         - Need a function that converts 0, 1, 2 to rock, paper, scissors
+
+
    3) Winner will be chosen based on the most wins out of five "throws"
 3) Need three radio buttons for user to select
 4) Need button to submit choice (rock, paper, scissors)
--- app.js
+++ app.js
@@ -1,8 +1,85 @@
 // import functions and grab DOM elements
-const input = document.getElementById('rock');
-const input = document.getElementById('paper');
-const input = document.getElementById('scissors');
-const button = document.getElementById('throw');
-// initialize state
+const throwButton = document.getElementById('throw');
+const resetButton = document.getElementById('reset-button');
 
-// set event listeners to update state and DOM
+const displayLastThrow = document.getElementById('last-throw');
+const dispalyWinLossDraw = document.getElementById('win-loss-draw');
+const displayWinnerLoser = document.getElementById('winner-loser');
+
+// initialize state
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
+
+
+
+// Throw Button Handler
+throwButton.addEventListener('click', () => {
+    const userThrow = document.querySelector(':checked').id;
+
+
+    // Get computers' choice
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerThrow
+
+    if (randomNumber === 0) {
+        computerThrow = 'rock';
+    } else if (randomNumber === 1) {
+        computerThrow = 'paper';
+    } else {
+        computerThrow = 'scissors';
+    }
+
+    
+    // Decides if User wins or Not
+    if (computerThrow === userThrow) {
+        displayLastThrow.textContent = 'result of last throw: Draw';
+        draws++;
+        displayWinnerLoser.textContent = 'winner/loser: No one!'
+    }
+    else if (computerThrow === 'rock' && userThrow === 'paper') {
+        displayLastThrow.textContent = 'result of last throw: Win';
+        wins ++;
+        displayWinnerLoser.textContent = 'winner/loser: User'
+    }
+    else if (computerThrow === 'rock' && userThrow === 'scissors') {
+        displayLastThrow.textContent = 'result of last throw: Loss';
+        losses++;
+        displayWinnerLoser.textContent = 'winner/loser: Computer'
+    }
+    else if (computerThrow === 'paper' && userThrow === 'rock') {
+        displayLastThrow.textContent = 'result of last throw: Loss';
+        losses++;
+        displayWinnerLoser.textContent = 'winner/loser: Computer'
+    }
+    else if (computerThrow === 'paper' && userThrow === 'scissors') {
+        displayLastThrow.textContent = 'result of last throw: Win';
+        wins++;
+        displayWinnerLoser.textContent = 'winner/loser: User'
+    }
+    else if (computerThrow === 'scissors' && userThrow === 'rock') {
+        displayLastThrow.textContent = 'result of last throw: Win';
+        wins++;
+        displayWinnerLoser.textContent = 'winner/loser: User'
+    }
+    else {
+        displayLastThrow.textContent = 'result of last throw: Loss';
+        losses++;
+        displayWinnerLoser.textContent = 'winner/loser: Computer'
+    }
+
+    dispalyWinLossDraw.textContent = `wins ${wins}, losses ${losses}, draws ${draws}`;
+})
+
+
+// Reset Button Handler
+resetButton.addEventListener('click', () => {
+    wins = 0;
+    losses = 0;
+    draws = 0;
+
+    displayWinnerLoser.textContent = 'winner/loser';
+    displayLastThrow.textContent = 'result of last throw';
+    dispalyWinLossDraw.textContent = 'wins 0, losses 0, draws 0';
+})
