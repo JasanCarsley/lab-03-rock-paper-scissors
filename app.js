02c1f30e5fa8fbb2c72f61c1bc2b67dacbec1bd1
JasanCarsley <jjcars@comcast.net>
2020-09-30 19:27:06 -0700

    added DOM

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // import functions and grab DOM elements
-
+const input = document.getElementById('rock');
+const input = document.getElementById('paper');
+const input = document.getElementById('scissors');
+const button = document.getElementById('throw');
 // initialize state
 
 // set event listeners to update state and DOM
